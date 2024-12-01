7dfe4ba139aca7be3df83825498b96874a5f8019
Sknsw <155487211+Sknsw@users.noreply.github.com>
2024-12-01 08:03:54 +0000

    guardar

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBIGj7-qtDP4pVDXpJ2ckmTMhO5x2KQVeA",
+  authDomain: "scort-glamur.firebaseapp.com",
+  projectId: "scort-glamur",
+  storageBucket: "scort-glamur.appspot.com",
+  messagingSenderId: "971144857909",
+  appId: "1:971144857909:web:353b6cb5590bc261c9647c"
 };
 
 export const app = initializeApp(firebaseConfig);
